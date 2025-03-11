33138393884ff57d3752125fcb37d844d52daac6
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-11 14:59:30 -0300

    desenvolvimento

--- Calabouco&Dragoes/Calabouco&Dragoes/Program.cs
+++ Calabouco&Dragoes/Calabouco&Dragoes/Program.cs
@@ -10,6 +10,54 @@ namespace Calabouco_Dragoes
     {
         static void Main(string[] args)
         {
+            string nome = "", classe = "", raca = "";
+            string resposta = "";
+            int vida = 100, dado_monstro, dado_jogador;
+            Random dado = new Random();
+
+            Console.WriteLine("Informe seu nome, aventureiro(a).");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Informe sua classe.");
+            classe = Console.ReadLine();
+
+            Console.WriteLine("Informe sua raça.");
+            raca = Console.ReadLine();
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Ficha: ");
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Classe: " + classe);
+            Console.WriteLine("Raça: " + raca);
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine("Deseja entrar na porta a sua esquerda? (S/N)");
+            resposta = Console.ReadLine();
+
+            if (resposta == "S")
+            {
+                Console.WriteLine("Aqui tem um monstro. Você vai enfrentá-lo!!!");
+
+                dado_monstro = dado.Next(1, 6);
+                dado_jogador = dado.Next(1, 6);
+
+                Console.WriteLine("Monstro: " + dado_monstro + " - Jogador: " + dado_jogador);
+
+                if (dado_monstro > dado_jogador)
+                {
+                    Console.WriteLine("Monstro Venceu!!!");
+                    vida = vida - 50;
+                }
+                else
+                {
+                    Console.WriteLine("Jogador Venceu!!!");
+                }
+
+            }
+            Console.WriteLine("Sua vida é: " + vida);
+
+            Console.WriteLine("Mais uma sala, você deseja entrar na sala? (S/N)");
+            resposta = Console.ReadLine();
         }
     }
 }
