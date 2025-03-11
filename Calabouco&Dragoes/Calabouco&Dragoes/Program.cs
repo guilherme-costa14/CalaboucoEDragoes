b15990cd08247a0e06c771da0961daa950565b4f
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-03-11 17:15:13 -0300

    Continuação
    Continuação da história, introduzindo novas dinâmicas e funcionalidades.
    Usar o ASCII... talvez.

--- Calabouco&Dragoes/Calabouco&Dragoes/Program.cs
+++ Calabouco&Dragoes/Calabouco&Dragoes/Program.cs
@@ -12,7 +12,7 @@ namespace Calabouco_Dragoes
         {
             string nome = "", classe = "", raca = "";
             string resposta = "";
-            int vida = 100, dado_monstro, dado_jogador;
+            int vida = 100, armadura = 100, dado_monstro, dado_jogador, dado_bau;
             Random dado = new Random();
 
             Console.WriteLine("Informe seu nome, aventureiro(a).");
@@ -87,6 +87,107 @@ namespace Calabouco_Dragoes
             }
 
             Console.ReadKey();
+
+            Console.WriteLine("Vocé se deparou com uma porta a direita... Deseja entrar? (S/N)");
+            resposta = Console.ReadLine();
+
+            if (resposta == "S")
+            {
+                Console.WriteLine("A sala que você entrou não tem nem um item de valor");
+                Console.WriteLine("Tem uma porta a frente, deseja entrar nela? (S/N)");
+                resposta = Console.ReadLine();
+                if (resposta == "S")
+                {
+                    Console.WriteLine("Você encontrou um báu.");
+                    dado_bau = dado.Next(1, 6);
+                    if (dado_bau == 1)
+                    {
+                        Console.WriteLine("Você tirou um item comum que vai te ajudar na sua jornada.");
+                    }
+                    else if (dado_bau == 2)
+                    {
+                        Console.WriteLine("Você tirou uma arma nova, mas este item também é comum.");
+                    }
+                    else if (dado_bau == 3)
+                    {
+                        Console.WriteLine("Que pena, o báu está vazio.");
+                    }
+                    else if(dado_bau == 4)
+                    {
+                        Console.WriteLine("Que pena, o báu está vazio.");
+                    }
+                    else if (dado_bau == 5)
+                    {
+                        Console.WriteLine("Você tirou um item lendário que vai te ajudar na sua jornada.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Infelizmente você não conseguiu nada que te ajude na sua aventura, siga em frente.");
+                    }
+                }
+            }
+
+            if (resposta == "N")
+            {
+                Console.WriteLine("Contínue a sua jornada");
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("Você encontrou o que parece ser uma salão antigo, deseja entrar? (S/N)");
+            resposta = Console.ReadLine();
+            if (resposta == "S")
+            {
+                Console.WriteLine("Eita, você se daparou com um monstro... prepare-se para enfrenta-lo");
+                dado_monstro = dado.Next(1, 6);
+                dado_jogador = dado.Next(1, 6);
+                
+                if(dado_monstro > dado_jogador)
+                {
+                    Console.WriteLine("Ele te acertou!!!");
+                    vida = vida - 40;
+                    Console.WriteLine("Vida " + vida);
+                }
+                
+                if (vida > 0)
+                {
+                    Console.WriteLine("Falta pouco... de mais um golpe ou fuja pela a sua vida... Fugir? (S/N)");
+                    resposta = Console.ReadLine();
+                    if(resposta == "S")
+                    {
+                        Console.WriteLine("Você é um bebê chorão T-T... e abandonou a luta");
+                    }
+
+                    if(resposta == "N")
+                    {
+                        Console.WriteLine("Você é um verdadeiro heroí.");
+                        dado_jogador = dado.Next(1, 6);
+                        dado_monstro = dado.Next(1, 6);
+
+                        if(dado_monstro > dado_jogador)
+                        {
+                            Console.WriteLine("Infelizmente a sua bravura não te recompensou... era melhor você ter fugido... FIM DE JOGO.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Você derrotou o monstro... como recompensa da sua coragem você ganhou um item novo.");
+                            Console.WriteLine("-----------------------------------------");
+                            Console.WriteLine("Item: Armadura -  " + armadura);
+                            Console.WriteLine("-----------------------------------------");
+                        }
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("A sua aventura chegou ao fim. " + vida);
+                }
+            }
+
+            Console.ReadKey();
+            Console.WriteLine("Contínue a sua jornada, explorando a área.");
+            Console.ReadKey();
+
+            Console.WriteLine();
         }
     }
 }
